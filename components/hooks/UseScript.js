e180af432861027906117ea95660ef2b9b432285
butterchoco <supri.contact@gmail.com>
2021-10-20 01:46:24 +0700

    refactor useScript

--- components/hooks/UseScript.js
+++ components/hooks/UseScript.js
@@ -1,8 +1,9 @@
 import useAsync from "./UseAsync";
 
-export default function useScript(url) {
+export default function useScript({ url, id }) {
   return useAsync(() => {
     const script = document.createElement("script");
+    if (id) script.id = id;
     script.src = url;
     script.async = true;
 
