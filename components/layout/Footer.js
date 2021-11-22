68948d5192ad58bb6e06260ab1a85825e6c41bbb
butterchoco <supri.contact@gmail.com>
2021-11-22 13:53:21 +0700

    fix social media && footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,24 +1,32 @@
-import { Box, Heading, HStack, Text } from "@chakra-ui/layout";
+import { Box, Heading, HStack, Text, VStack } from "@chakra-ui/layout";
+import { useDataBackend } from "../context/DataContext";
+import useWindowSize from "../hooks/UseWindowSize";
 import SocialMedia from "../reusables/SocialMedia";
 
 const Footer = () => {
+  const { isLaptopDisplay, isTabletDisplay } = useWindowSize();
+  const { about } = useDataBackend();
+
+  if (!about) return null;
+
   return (
     <Box
       backgroundColor="#000"
-      p="2%"
+      py={isTabletDisplay ? "10px" : "4rem"}
+      pl={isLaptopDisplay ? "2rem" : "10%"}
+      pr={isLaptopDisplay ? "1rem" : "10%"}
       display="flex"
-      flexDir="column"
       alignItems="center"
-      justifyContent="center"
+      justifyContent="space-between"
       color="white"
       gridGap="1rem"
     >
-      <HStack width="100%" justifyContent="center" alignItems="center">
+      <VStack width="50%" justifyContent="center" alignItems="flex-start">
         <Heading as="h4" fontSize="14px">
-          BengkelBagus
+          {about.companyName}
         </Heading>
-        <Text fontSize="14px">Copyright 2021</Text>
-      </HStack>
+        <Text fontSize="14px">{about.companyDescription}</Text>
+      </VStack>
       <Box>
         <SocialMedia />
       </Box>
--- components/reusables/SocialMedia.js
+++ components/reusables/SocialMedia.js
@@ -1,14 +1,26 @@
 import { BACKEND_URL } from "@/Utils/Constants";
-import { Box, HStack, Text, Image } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Text,
+  Image,
+  VStack,
+  useClipboard,
+} from "@chakra-ui/react";
 import styled from "@emotion/styled";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const SocMedContainer = styled.div`
+  width: 42px;
+  height: 42px;
   cursor: pointer;
   border-width: 2px;
   border-color: transparent;
   border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   transition: border-color 0.5s ease;
 
   &:hover {
@@ -18,6 +30,9 @@ const SocMedContainer = styled.div`
 
 const SocialMedia = () => {
   const [socialMedia, setSocialMedia] = useState([]);
+  const [phoneNumber, setPhoneNumber] = useState(null);
+  const [emailData, setEmailData] = useState(null);
+  const { hasCopied, onCopy } = useClipboard(phoneNumber);
 
   useEffect(() => {
     fetchApi();
@@ -26,25 +41,43 @@ const SocialMedia = () => {
   const fetchApi = async () => {
     await fetch(BACKEND_URL + "/social-medias")
       .then((res) => res.json())
-      .then((data) => setSocialMedia(data))
+      .then((data) => {
+        const phoneData = data.find((data) => data.name === "phone");
+        if (phoneData) setPhoneNumber(phoneData.url);
+        const emailData = data.find((data) => data.name === "email");
+        if (emailData) setEmailData(emailData.url);
+        setSocialMedia(data);
+      })
       .catch((error) => error);
   };
 
+  console.log(hasCopied);
+
   return (
-    <HStack width="100%" gridGap="1rem">
-      {socialMedia.length !== 0 &&
-        socialMedia.map((data, index) => (
-          <Link key={index} scroll={false} href={data.url} passHref>
-            <SocMedContainer>
-              <Image
-                boxSize="42px"
-                src={`/images/${data.name}-icon.svg`}
-                alt={data.name}
-              />
-            </SocMedContainer>
-          </Link>
-        ))}
-    </HStack>
+    <VStack>
+      <HStack width="100%" gridGap="1rem">
+        {socialMedia.length !== 0 &&
+          socialMedia.map((data, index) => {
+            if (data.name === "phone") {
+              return null;
+            }
+            return (
+              <Link key={index} scroll={false} href={data.url} passHref>
+                <SocMedContainer>
+                  <Image
+                    src={`/images/${data.name}-icon.svg`}
+                    alt={data.name}
+                  />
+                </SocMedContainer>
+              </Link>
+            );
+          })}
+      </HStack>
+      <VStack width="100%" alignItems="flex-end">
+        {emailData && <Text>{emailData.split(":")[1]}</Text>}
+        {phoneNumber && <Text>{phoneNumber}</Text>}
+      </VStack>
+    </VStack>
   );
 };
 
